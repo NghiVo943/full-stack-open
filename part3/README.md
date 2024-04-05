894b36c0ed8c236817659facd5e888771d897616
nghivo-aalto <105486592+NghiVo943@users.noreply.github.com>
2024-04-05 11:36:10 +0000

    mongodb integration

---
+++ part3/README.md
@@ -0,0 +1,2 @@
+### Web service link:
+https://full-stack-open-0z49.onrender.com
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
+
+const Person = require('./models/person')
 
 const app = express()
 app.use(cors())
@@ -16,59 +19,53 @@ app.use(morgan(function (tokens, req, res) {
 
 app.use(express.static('dist'))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+    // { 
+    //   "id": 1,
+    //   "name": "Arto Hellas", 
+    //   "number": "040-123456"
+    // },
+    // { 
+    //   "id": 2,
+    //   "name": "Ada Lovelace", 
+    //   "number": "39-44-5323523"
+    // },
+    // { 
+    //   "id": 3,
+    //   "name": "Dan Abramov", 
+    //   "number": "12-43-234345"
+    // },
+    // { 
+    //   "id": 4,
+    //   "name": "Mary Poppendieck", 
+    //   "number": "39-23-6423122"
+    // }
 ]
 
 app.use(express.json())
 
 app.get('/info', (request, response) => {
-  const timestamp = new Date()
-  response.send(`<div>
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${timestamp}</div>
-  </div>`)  
+  Person.find({}).then(persons => {
+    const timestamp = new Date()
+    response.send(`<div>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${timestamp}</div>
+    </div>`)
+  })  
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  }
-  else {
-    response.status(404).end()
-  }
-})
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  
-  response.status(204).end()
+  Person.find({}).then(persons => response.json(persons))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -86,15 +83,43 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
+  const person = new Person({
+    name: body.name,
+    number: body.number  
+  })
+  person.save().then(savedPerson => { response.json(savedPerson) })
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        return response.json(person)
+      }
+      response.status(404).end()
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => response.status(204).end())
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
   const person = {
     name: body.name,
-    number: body.number,
-    id: Math.round(Math.random()*1000000000),
+    number: body.number  
   }
-
-  persons = persons.concat(person)
-  response.json(person)
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => response.json(updatedPerson))
+    .catch(error => next(error))
 })
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
