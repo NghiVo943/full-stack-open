b275ec0691432271d3de23922a67763299dd54f1
Nghi Vo <nghi.vodong942003@gmail.com>
2024-04-06 20:09:24 +0300

    setups

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -3,8 +3,19 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
