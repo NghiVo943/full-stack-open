d4313b209f6668372444f92bdb1504b19557792c
Nghi Vo <nghi.vodong942003@gmail.com>
2024-04-18 19:00:28 +0300

    testing router

--- part5/bloglist-backend/app.js
+++ part5/bloglist-backend/app.js
@@ -8,9 +8,9 @@ const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 
-const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const blogsRouter 	= require('./controllers/blogs')
+const usersRouter 	= require('./controllers/users')
+const loginRouter 	= require('./controllers/login')
 
 const app = express()
 
@@ -32,6 +32,11 @@ app.use(express.json())
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
+
+if (process.env.NODE_ENV === 'test') {
+	const testingRouter = require('./controllers/testing')
+	app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
